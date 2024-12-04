55a89c99ddc98c81e2cbcb22db652c8cf7b6648f
Faizimughal <162406605+Faizimughal@users.noreply.github.com>
2024-12-04 18:41:53 +0500

    changings

--- script.js
+++ script.js
@@ -2,11 +2,36 @@ let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 let miliseconds = document.getElementById("miliseconds");
 
+let pauseBtn = document.getElementById("pause");
+let resetBtn = document.getElementById("reset");
+let startBtn = document.getElementById("start");
+
 newDate = () => {
   let date = new Date();
   minutes.innerHTML = date.getMinutes();
   seconds.innerHTML = date.getSeconds();
   miliseconds.innerHTML = date.getMilliseconds();
-  setInterval(newDate, 1000);
 };
-newDate();
+setInterval(() => {
+  newDate();
+}, 1000);
+
+resetBtn.addEventListener("click", () => {
+  clearInterval(newDate());
+  newDate = () => {
+    let date = new Date();
+    minutes.innerHTML = "00";
+    seconds.innerHTML = "00";
+    miliseconds.innerHTML = "00";
+  };
+});
+
+startBtn.addEventListener("click", () => {
+  clearInterval(newDate());
+  newDate = () => {
+    let date = new Date();
+    minutes.innerHTML = date.getMinutes();
+    seconds.innerHTML = date.getSeconds();
+    miliseconds.innerHTML = date.getMilliseconds();
+  };
+});
